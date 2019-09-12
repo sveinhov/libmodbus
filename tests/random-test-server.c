8f6598fb2ca7376a6148aef7328dd0d74c15a4b0
sveinhov <47802108+sveinhov@users.noreply.github.com>
2019-09-12 07:12:29 -0500

    Successful connection to PLC

--- tests/random-test-server.c
+++ tests/random-test-server.c
@@ -15,12 +15,51 @@
 
 int main(void)
 {
-    int s = -1;
+
+	modbus_t *ctx;
+	uint16_t tab_reg[64];
+	int rc;
+	int i;
+
+	ctx = modbus_new_tcp("192.168.51.31", 502);
+	if (modbus_connect(ctx) == -1) {
+		fprintf(stderr, "Connection failed: %s\n", modbus_strerror(errno));
+		modbus_free(ctx);
+		return -1;
+	}
+	//while (;;){}
+	rc = modbus_read_registers(ctx, 12388, 10, tab_reg);
+	if (rc == -1) {
+		fprintf(stderr, "%s\n", modbus_strerror(errno));
+		return -1;
+	}
+	
+	for (i = 0; i < rc; i++) {
+		printf("reg[%d]=%d (0x%X)\n", i, tab_reg[i], tab_reg[i]);
+	}
+	
+
+	modbus_close(ctx);
+	modbus_free(ctx);
+
+	/* modbus_t *mb;
+  uint16_t tab_reg[32];
+
+  mb = modbus_new_tcp("127.0.0.1", 1502);
+  modbus_connect(mb);
+
+  // Read 5 registers from the address 0
+	modbus_read_registers(mb, 0, 5, tab_reg);
+
+	modbus_close(mb);
+	modbus_free(mb); */
+
+    /*int s = -1;
     modbus_t *ctx;
     modbus_mapping_t *mb_mapping;
 
-    ctx = modbus_new_tcp("127.0.0.1", 1502);
-    /* modbus_set_debug(ctx, TRUE); */
+    ctx = modbus_new_tcp("192.168.50.31", 1502);
+    // modbus_set_debug(ctx, TRUE); 
 
     mb_mapping = modbus_mapping_new(500, 500, 500, 500);
     if (mb_mapping == NULL) {
@@ -39,10 +78,10 @@ int main(void)
 
         rc = modbus_receive(ctx, query);
         if (rc > 0) {
-            /* rc is the query size */
+            // rc is the query size 
             modbus_reply(ctx, query, rc, mb_mapping);
         } else if (rc == -1) {
-            /* Connection closed by the client or error */
+            // Connection closed by the client or error 
             break;
         }
     }
@@ -55,6 +94,6 @@ int main(void)
     modbus_mapping_free(mb_mapping);
     modbus_close(ctx);
     modbus_free(ctx);
-
+	*/
     return 0;
 }
