8bea1fe6d24378dd26cbb5a83cd8c602fbc375d9
sveinhov <47802108+sveinhov@users.noreply.github.com>
2019-09-12 09:41:33 -0500

    Reading correct values except Word
    Reading correct values from PLC except for a couple of Word registers

--- tests/random-test-server.c
+++ tests/random-test-server.c
@@ -13,11 +13,17 @@
 
 #include <modbus.h>
 
+// basic file operations
+//#include <iostream>
+//#include <fstream>
+//using namespace std;
+
 int main(void)
 {
-
 	modbus_t *ctx;
-	uint16_t tab_reg[64];
+	//allocate enough memory to store the results in dest (at least nb * sizeof(uint16_t)), = nb * 2.
+	uint16_t tab_reg[80];
+	uint16_t sample[2];
 	int rc;
 	int i;
 
@@ -28,72 +34,32 @@ int main(void)
 		return -1;
 	}
 	//while (;;){}
-	rc = modbus_read_registers(ctx, 12388, 10, tab_reg);
+	rc = modbus_read_registers(ctx, 12388, 40, tab_reg);
 	if (rc == -1) {
 		fprintf(stderr, "%s\n", modbus_strerror(errno));
 		return -1;
 	}
 	
-	for (i = 0; i < rc; i++) {
+	FILE *f = fopen("raw.txt", "w");
+	FILE *fo = fopen("out.txt", "w");
+
+	for (i = 0; i < rc; i+=2) {
+				
 		printf("reg[%d]=%d (0x%X)\n", i, tab_reg[i], tab_reg[i]);
+		fprintf(f, "%u \n%u \n", tab_reg[i], tab_reg[i+1]);
+		
+		float test = 0;
+		sample[0] = tab_reg[i];
+		sample[1] = tab_reg[i+1];
+		test = modbus_get_float_badc(sample);
+		fprintf(fo, "%f \n", test);
 	}
 	
+	fclose(f);
+	fclose(fo);
 
 	modbus_close(ctx);
 	modbus_free(ctx);
 
-	/* modbus_t *mb;
-  uint16_t tab_reg[32];
-
-  mb = modbus_new_tcp("127.0.0.1", 1502);
-  modbus_connect(mb);
-
-  // Read 5 registers from the address 0
-	modbus_read_registers(mb, 0, 5, tab_reg);
-
-	modbus_close(mb);
-	modbus_free(mb); */
-
-    /*int s = -1;
-    modbus_t *ctx;
-    modbus_mapping_t *mb_mapping;
-
-    ctx = modbus_new_tcp("192.168.50.31", 1502);
-    // modbus_set_debug(ctx, TRUE); 
-
-    mb_mapping = modbus_mapping_new(500, 500, 500, 500);
-    if (mb_mapping == NULL) {
-        fprintf(stderr, "Failed to allocate the mapping: %s\n",
-                modbus_strerror(errno));
-        modbus_free(ctx);
-        return -1;
-    }
-
-    s = modbus_tcp_listen(ctx, 1);
-    modbus_tcp_accept(ctx, &s);
-
-    for (;;) {
-        uint8_t query[MODBUS_TCP_MAX_ADU_LENGTH];
-        int rc;
-
-        rc = modbus_receive(ctx, query);
-        if (rc > 0) {
-            // rc is the query size 
-            modbus_reply(ctx, query, rc, mb_mapping);
-        } else if (rc == -1) {
-            // Connection closed by the client or error 
-            break;
-        }
-    }
-
-    printf("Quit the loop: %s\n", modbus_strerror(errno));
-
-    if (s != -1) {
-        close(s);
-    }
-    modbus_mapping_free(mb_mapping);
-    modbus_close(ctx);
-    modbus_free(ctx);
-	*/
     return 0;
 }
